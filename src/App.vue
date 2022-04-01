ee37591df89aa527d1ebea33800d4bee952063a1
Glabdugrib <si.sada93@gmail.com>
2022-04-01 23:03:13 +0200

    Header completed & scroll-up button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
    <div id="app">
+
       <Header />
+
+      <!-- Da far apparire solo in modalità desktop (non mobile) -->
+      <a href="#" class="scroll-up-btn"> 
+         <i class="fa-solid fa-arrow-up"></i>
+      </a>
+
    </div>
 </template>
 
@@ -18,12 +25,12 @@ export default {
 <style lang="scss">
 @import './assets/scss/_variables.scss';
 @import url('https://fonts.googleapis.com/css2?family=Teko:wght@600&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap');
 
 #app {
 //   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  font-family: 'Teko', sans-serif;
 }
 
 * {
@@ -32,8 +39,21 @@ export default {
    margin: 0;
 }
 
+html {
+   scroll-behavior: smooth;
+   // aggiungere funzionalità per altri browser
+   user-select: none;
+   
+   &::-webkit-scrollbar {
+      display: none;
+   }
+}
+
 body {
    background-color: $main-bg;
+   cursor: default;
+   position: relative;
+   height: 200vh;
 }
 
 .container {
@@ -44,6 +64,32 @@ body {
 img {
    display: block;
    max-width: 100%;
+}
+
+a {
+   color: currentColor;
+   text-decoration: none;
+}
+
+.scroll-up-btn {
+   position: fixed;
+   bottom: 15px;
+   right: 15px;
+   width: 50px;
+   aspect-ratio: 1;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   // border: 1px solid black;
+   border-radius: 5px;
+   font-size: 20px;
+   color: white;
+   background-color: rgba($color: black, $alpha: 0.3);
+   opacity: 0.5;
+
+   &:hover {
+      opacity: 1;
+   }
 }
 
 </style>
--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -6,6 +6,16 @@
          </figure>
          <h1 class="logo-title">YearlyPlanner</h1>
       </div>
+      <div class="header-menu">
+         <a href="#" class="header-menu-btn">
+            <i class="fa-solid fa-list-check"></i>
+            <span>Other Project</span>
+         </a>
+         <a href="#" class="header-menu-btn">
+            <i class="fa-solid fa-right-to-bracket"></i>
+            <span>Login</span>
+         </a>
+      </div>
    </header>
 </template>
 
@@ -19,7 +29,9 @@ export default {
 @import '../assets/scss/_variables.scss';
 
 header {
+   padding: 30px 0;
    display: flex;
+   flex-direction: column;
    justify-content: center;
    border: 1px solid white;
 
@@ -28,13 +40,13 @@ header {
       justify-content: center;
       align-items: center;
       gap: 10px;
-      padding: 0 100px;
-      margin: 0 auto;
+      // padding: 0 50px;
+      margin: 0 auto 20px auto;
       transition: letter-spacing ease-in-out 0.3s;
-      border: 1px solid red;
+      // border: 1px solid red;
 
       &:hover {
-         letter-spacing: 5px;
+         letter-spacing: 5px; //rimuovere l'effetto nel mobile
       }
 
       .logo-wrapper {
@@ -47,14 +59,70 @@ header {
       }
 
       .logo-title {
+         font-family: 'Teko', sans-serif;
          font-size: 56px;
          font-weight: 600;
          color: $text-light;
          padding-top: 12px;
       }
    }
+
+   .header-menu {
+      display: flex;
+      justify-content: center;
+      gap: 10px;
+      font-family: 'Lato', sans-serif;
+      // border: 1px solid red;
+
+      .header-menu-btn {
+         padding: 5px 10px;
+         display: flex;
+         align-items: center;
+         color: rgba(255, 255, 255, 0.75);
+         background-color: $secundary-bg-light;
+         border-radius: 3px;
+
+         &:hover {
+            color: white;
+         }
+
+         i {
+            font-size: 18px;
+            margin-right: 5px;
+         }
+
+         span {
+            font-size: 14px;
+            font-weight: 700;
+            text-transform: uppercase;
+         }
+      }
+   }
 }
 
+// M
+@media screen and (max-width: 768px) {
+   header {
 
+      .main-title {
+
+         .main-title {
+            font-size: 51px;
+         }
+      }
+   }
+}
+
+@media screen and (max-width: 478px) {
+   header {
+
+      .main-title {
+
+         &:hover {
+            letter-spacing: initial;
+         }
+      }
+   }
+}
 
 </style>
